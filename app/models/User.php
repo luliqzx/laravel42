a908bb4db0abb191a974a202c0614113b7eeffc5
luliqzx <luliqzx@gmail.com>
2016-06-29 16:08:28 +0700

    update

--- app/models/User.php
+++ app/models/User.php
@@ -7,20 +7,36 @@ use Illuminate\Auth\Reminders\RemindableInterface;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
-	use UserTrait, RemindableTrait;
+	// use UserTrait, RemindableTrait;
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
+	// /**
+	//  * The database table used by the model.
+	//  *
+	//  * @var string
+	//  */
+	// protected $table = 'users';
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password', 'remember_token');
+	// /**
+	//  * The attributes excluded from the model's JSON form.
+	//  *
+	//  * @var array
+	//  */
+	// protected $hidden = array('password', 'remember_token');
+
+	public function getAuthIdentifier() {
+		return $this->getKey();
+	}
+	public function getAuthPassword() {
+		return $this->password;
+	}
+	public function cats(){
+		return $this->hasMany('Cat');
+	}
+	public function owns(Cat $cat){
+		return $this->id == $cat->owner;
+	}
+	public function canEdit(Cat $cat){
+		return $this->is_admin or $this->owns($cat);
+	}
 
 }
